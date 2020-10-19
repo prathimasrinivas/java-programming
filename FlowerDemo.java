351ea23ce75fe3bb25b77dffc10e33223a8e2260
prathimasrinivas <72918251+prathimasrinivas@users.noreply.github.com>
2020-10-19 10:48:48 +0530

    Add files via upload
    program to display states and objects of flowers using classes and objects

---
+++ FlowerDemo.java
@@ -0,0 +1,22 @@
+
+
+class FlowerDemo{
+
+public static void main(String args[]){
+
+Flower rose=new Flower();
+rose.name="rose";
+rose.color="red";
+rose.blooming();
+
+Flower jasmine=new Flower();
+jasmine.name="jasmine";
+jasmine.color="white";
+jasmine.blooming();
+
+Flower lotus=new Flower();
+lotus.name="lotus";
+lotus.color="pink";
+lotus.blooming();
+}
+}
