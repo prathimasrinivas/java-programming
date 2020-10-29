0365ab69555a6410058d0caaf5ee995b39148925
prathimasrinivas <72918251+prathimasrinivas@users.noreply.github.com>
2020-10-29 11:34:35 +0530

    Add files via upload
    programs to display the numbers in rows and columns in particular patterns

---
+++ Numberpattern.java
@@ -0,0 +1,12 @@
+class NumberPattern{
+public static void main(String args[]){
+int count=0;
+for(int i=1; i<=3; i++){
+for(int j=1; j<=3; j++){
+count=count+1;
+System.out.print(count + " ");
+}
+System.out.println();
+}
+}
+}
