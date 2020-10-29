71f909acf2d73d926b430f6ae03557207e551066
prathimasrinivas <72918251+prathimasrinivas@users.noreply.github.com>
2020-10-29 11:27:27 +0530

    Add files via upload
    program to display numbers in rows and columns using different patterns

---
+++ NestedForLoopDemo.java
@@ -0,0 +1,12 @@
+class NestedForLoopDemo{
+public static void main(String args[]){
+
+for(int i=1; i<=3; i++){
+for(int j=1; j<=i+1; j++){
+
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
